9392489183a394c374c2dc242a3f1055bd7dec58
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-11-17 11:55:40 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame;
+
+from checkers.constants import WIDTH, HEIGHT;
+from checkers.board import Board;
+
+
+FPS = 60
+
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+#eventloop
+def main():
+    run = True
+    clock = pygame.time.Clock() #base clock speed depending on computer. 
+    board = Board
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        board.draw_squares(WIN)
+        pygame.display.update() 
+
+
+
+    pygame.quit()
+
+main()
